91065e44fe30ad5c570467f0936c0edf4e649d1a
Nutthanut Kijboonchoo <toykung.tv47@gmail.com>
2025-01-19 01:08:27 +0700

    First ui 6530202668

--- app/src/main/java/scisrc/mobiledev/firstuiassignment/RegisterActivity.kt
+++ app/src/main/java/scisrc/mobiledev/firstuiassignment/RegisterActivity.kt
@@ -1,7 +1,13 @@
 package scisrc.mobiledev.firstuiassignment
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StyleSpan
+import android.graphics.Typeface
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,10 +27,27 @@ class RegisterActivity : AppCompatActivity() {
             insets
         }
 
+        val signUpTextView = findViewById<TextView>(R.id.textView2)
+
+        val text = "Don't have an account? Sign Up"
+
+        val spannable = SpannableString(text)
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        spannable.setSpan(boldSpan, text.indexOf("Sign Up"), text.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        signUpTextView.text = spannable
+
+        signUpTextView.setOnClickListener {
+            // Start the LoginActivity when the TextView is clicked
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
         backToMainPageBtn = findViewById(R.id.registBackToMainBtn)
 
         backToMainPageBtn.setOnClickListener() {
-            finish()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
